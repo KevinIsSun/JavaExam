08d07cccaad283ac2ceaead554bb00e7dc995cc5
kevinissun <kevinissun@163.com>
2020-12-10 21:18:05 +0800

    add bug controller

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ register and login. When user registers, they need provide their username, email
 login, please use cookie to keep the user login status, and it will expire after half an hour.
 2. Complete the unit test for the functions implemented in the first step, including tests for the controller and
 service, etc. Try your best to increase code coverage.
+3. Fix bugs in BugController. Make sure we can see "Success" when open "/show_bug" page.
+4. After you completed, please send a pull request to origin repo.
 
--- src/main/java/com/example/springboot/controller/BugController.java
+++ src/main/java/com/example/springboot/controller/BugController.java
@@ -4,11 +4,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RestController
 @Slf4j
 public class BugController {
-    @RequestMapping("/")
+    private Integer num;
+
+    class AddThread implements Callable<Integer> {
+        private Integer num;
+
+        public AddThread(Integer n) {
+            num = n;
+        }
+
+        public Integer call() {
+            for (int i=0; i<50; i++) {
+                num += i;
+            }
+            return num;
+        }
+    }
+
+    @RequestMapping("/show_bug")
     public String showBug() {
-        return "index";
+        try {
+            ExecutorService executor = Executors.newCachedThreadPool();
+            List<Future<Integer>> futureList = new ArrayList<>();
+            for (int i=0; i<4; i++) {
+                Future<Integer> future = executor.submit(new AddThread(num));
+                futureList.add(future);
+            }
+            int total = 0;
+            for (int i=0; i<4; i++) {
+                total += futureList.get(i).get();
+            }
+            if (total != 200) {
+                return "Invalid result";
+            }
+            return "Success";
+        } catch (Exception e) {
+            log.error("Exception: " + e.getMessage());
+            return "Exception";
+        }
     }
 }
